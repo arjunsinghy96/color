be4ec953ad6ebcb97cf876c5aeb4561de0f35a13
lavee singh <laveechaudharytheone@gmail.com>
2017-03-20 19:09:30 +0000

    Added ColorString class and tests for it

--- colorit.py
+++ colorit.py
@@ -57,3 +57,61 @@ def strike(msg):
     return: text wrapped in ANSI escape code for strikethrough
     """
     return ATTR['strike'] + msg + RESET
+
+class ColorString(str):
+    
+    RESET = '\033[0m'
+
+    ATTR = { 'bold' : '\033[1m',
+        'underline' : '\033[4m',
+         'strike' : '\033[9m' 
+     }
+
+    def __init__(self, text):
+        self.text = text
+
+    def color_front(self, rgb):
+        """
+        S.color_front(rgb) -> string
+        rgb: tuple containing rgb values
+
+        print front coloured string.
+        """
+        red, green, blue = rgb
+        foreground = '\033[38;2;{r};{g};{b}m'.format(r=red, g=green, b=blue)
+        return ColorString(foreground + self.text + self.RESET)
+
+    def color_back(self, rgb):
+        """
+        S.color_back(rgb) -> string
+        rgb: tuple containing rgb values
+        
+        print background coloured string.
+        """
+        red, green, blue = rgb
+        foreground = '\033[48;2;{r};{g};{b}m'.format(r=red, g=green, b=blue)
+        return ColorString(foreground + self.text + self.RESET)
+
+    def bold(self):
+        """
+        S.bold() -> string
+
+        print bold string.
+        """
+        return ColorString(self.ATTR['bold'] + self.text + RESET)
+
+    def under(self):
+        """
+        S.under() -> string
+
+        print underlined string.
+        """
+        return ColorString(self.ATTR['underline'] + self.text + RESET)
+
+    def strike(self):
+        """
+        S.strike() -> string
+
+        print strikedthrough string.
+        """
+        return ColorString(self.ATTR['strike'] + self.text + RESET)

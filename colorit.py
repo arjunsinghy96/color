31e23645234cc4cfc29a7b5cbc227023222b9fbb
lavee singh <laveechaudharytheone@gmail.com>
2017-07-15 17:01:33 +0530

    Added ColorText class, with basic work

--- colorit.py
+++ colorit.py
@@ -7,6 +7,62 @@ ATTR = { 'bold' : '\033[1m',
          'strike' : '\033[9m' 
          }
 
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+STRIKE = "\033[9m"
+
+class ColorText(str):
+
+    def __init__(self, text=None):
+        self.text = text
+        self.decorated = text
+
+    def color_front(self, colorname=None, rgb=None, hexvalue=None):
+        if not (colorname or rgb or hexvalue):
+            raise Exception("Please specify a color parameter")
+        if colorname:
+            rgb = str_to_rgb(colorname)
+        if hexvalue:
+            rgb = hex_to_rgb(hexvalue)
+        red, green, blue = rgb
+        foreground = '\033[38;2;{r};{g};{b}m'.format(r=red, g=green, b=blue)
+        self.decorated = foreground + self.decorated + RESET
+        return ColorText(self.decorated)
+
+    def color_back(self, colorname=None, rgb=None, hexvalue=None):
+        if not (colorname or rgb or hexvalue):
+            raise Exception("Please specify a color parameter")
+        if colorname:
+            rgb = str_to_rgb(colorname)
+        if hexvalue:
+            rgb = hex_to_rgb(hexvalue)
+        red, green, blue = rgb
+        background = '\033[48;2;{r};{g};{b}m'.format(r=red, g=green, b=blue)
+        self.decorated = background + self.decorated + RESET
+        return ColorText(self.decorated)
+
+    def bold(self):
+        self.decorated = BOLD + self.text + RESET
+        return ColorText(self.decorated)
+
+    def underline(self):
+        self.decorated = UNDERLINE + self.decorated + RESET
+        return ColorText(self.decorated)
+
+    def strike(self):
+        self.decorated = STRIKE + self.decorated + RESET
+        return ColorText(self.decorated)
+
+    def __unicode__(self):
+        return self.decorated
+
+    def __str__(self):
+        return self.decorated
+
+    def raw(self):
+        return self.decorated
+
+
 def color_front(text, red, green, blue):
     """
     wraps the text in appropriate ANSI escape code for foreground color
